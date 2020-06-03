b68bc83a861528afb26fbe2d68cf9e934def6932
¨Levi <levilude@gmail.com>
2020-06-03 14:29:46 -0300

    Comentario de classe

--- src/main/java/com/lude/sisempresa/resources/CategoriaResource.java
+++ src/main/java/com/lude/sisempresa/resources/CategoriaResource.java
@@ -19,6 +19,7 @@ public class CategoriaResource {
 		Categoria cat1 = new Categoria(1, "Informática");
 		Categoria cat2 = new Categoria(2, "Escritório");
 		
+		//Criação de lista
 		List<Categoria> lista = new ArrayList<>();
 		lista.add(cat1);
 		lista.add(cat2);
