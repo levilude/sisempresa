36644a6d331b6205e6a9ae49a545554f20675bf8
¨Levi <levilude@gmail.com>
2020-06-03 12:42:48 -0300

    Projeto criado

---
+++ src/main/java/com/lude/sisempresa/SisempresaApplication.java
@@ -0,0 +1,13 @@
+package com.lude.sisempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisempresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisempresaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lude/sisempresa/SisempresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lude.sisempresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisempresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
