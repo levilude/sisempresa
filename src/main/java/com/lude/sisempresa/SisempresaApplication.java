172a8c9397a4873327f07e00672c2541a10c63f0
¨Levi <levilude@gmail.com>
2020-06-04 17:14:57 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/lude/sisempresa/SisempresaApplication.java
+++ src/main/java/com/lude/sisempresa/SisempresaApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.lude.sisempresa.domain.Categoria;
+import com.lude.sisempresa.domain.Produto;
 import com.lude.sisempresa.repositories.CategoriaRepository;
+import com.lude.sisempresa.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SisempresaApplication implements CommandLineRunner{
@@ -19,6 +21,9 @@ public class SisempresaApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -26,7 +31,24 @@ public class SisempresaApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().add(p2);
+		
+		p1.getCategorias().add(cat1);
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().add(cat1);
+		
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		
 		
 	}
 
