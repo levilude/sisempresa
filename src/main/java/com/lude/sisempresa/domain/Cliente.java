78b857b4ce9a86515153cb51ac4179f34fad534e
¨Levi <levilude@gmail.com>
2020-06-05 21:24:04 -0300

    Endpoint /clientes/{id}

--- src/main/java/com/lude/sisempresa/domain/Cliente.java
+++ src/main/java/com/lude/sisempresa/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.lude.sisempresa.domain.enums.TipoCliente;
 
 @Entity
@@ -28,6 +29,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente") //Foi mapeado pelo campo cliente na classe (Endereco)
 	private List<Endereco> enderecos = new ArrayList<>();
 	
---
+++ src/main/java/com/lude/sisempresa/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.lude.sisempresa.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lude.sisempresa.domain.Cliente;
+import com.lude.sisempresa.repositories.ClienteRepository;
+import com.lude.sisempresa.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	// Instanciar objeto por injeção de dependencia. O spring faz isso automático
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id));
+	}
+
+}
