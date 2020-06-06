1609395343dbefea6342bde671a41064bb95dec5
¨Levi <levilude@gmail.com>
2020-06-06 17:27:02 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/lude/sisempresa/resources/CategoriaResource.java
+++ src/main/java/com/lude/sisempresa/resources/CategoriaResource.java
@@ -21,13 +21,15 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
+	//OBTER CATEGORIA
 	//Resposta http com corpo (ResponseEntity<?>)
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);	
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	//SALVAR CATEGORIA
 	//Resposta http sem corpo ResponseEntity<void>
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
@@ -39,5 +41,17 @@ public class CategoriaResource {
 		
 		return ResponseEntity.created(uri).build();	
 	}
+	
+	//ATUALIZAR CATEGORIA
+	//Resposta http sem corpo ResponseEntity<void>
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 	
 }
