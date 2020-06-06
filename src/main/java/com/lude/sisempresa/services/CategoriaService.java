eec428fd4ea59742cf2799128a8f6a30d5514891
¨Levi <levilude@gmail.com>
2020-06-06 19:31:21 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/lude/sisempresa/services/CategoriaService.java
+++ src/main/java/com/lude/sisempresa/services/CategoriaService.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.lude.sisempresa.domain.Categoria;
-import com.lude.sisempresa.dto.CategoriaDTO;
 import com.lude.sisempresa.repositories.CategoriaRepository;
 import com.lude.sisempresa.services.exceptions.DataIntegrityException;
 import com.lude.sisempresa.services.exceptions.ObjectNotFoundException;
@@ -48,6 +50,13 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	//Buscar categorias com paginação
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
 	}
 
 }
