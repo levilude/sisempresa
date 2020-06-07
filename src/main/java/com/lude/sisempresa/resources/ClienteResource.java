91cf9fe5d7af38605590f812eb51e910d8173e1b
¨Levi <levilude@gmail.com>
2020-06-07 14:24:42 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/lude/sisempresa/resources/ClienteResource.java
+++ src/main/java/com/lude/sisempresa/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.lude.sisempresa.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lude.sisempresa.domain.Cliente;
 import com.lude.sisempresa.dto.ClienteDTO;
+import com.lude.sisempresa.dto.ClienteNewDTO;
 import com.lude.sisempresa.services.ClienteService;
 
 @RestController
@@ -32,6 +35,18 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		
+		//Boa prática (Devolver código HTTP padrão para inserção com sucesso (RETORNAR O CÓDIGO --> 201 - Created e a URI do novo recurso)
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();	
+	}
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
 		Cliente obj = service.fromDTO(objDto);
--- src/main/java/com/lude/sisempresa/services/ClienteService.java
+++ src/main/java/com/lude/sisempresa/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.lude.sisempresa.domain.Cidade;
 import com.lude.sisempresa.domain.Cliente;
+import com.lude.sisempresa.domain.Endereco;
+import com.lude.sisempresa.domain.enums.TipoCliente;
 import com.lude.sisempresa.dto.ClienteDTO;
+import com.lude.sisempresa.dto.ClienteNewDTO;
 import com.lude.sisempresa.repositories.ClienteRepository;
+import com.lude.sisempresa.repositories.EnderecoRepository;
 import com.lude.sisempresa.services.exceptions.DataIntegrityException;
 import com.lude.sisempresa.services.exceptions.ObjectNotFoundException;
 
@@ -22,6 +28,9 @@ public class ClienteService {
 	// Instanciar objeto por injeção de dependencia. O spring faz isso automático
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -29,17 +38,26 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+
+	}
+
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
 		return repo.save(newObj);
 	}
-	
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
@@ -47,23 +65,41 @@ public class ClienteService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
 		}
-		
+
 	}
-	
+
 	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
-	
-	//Buscar categorias com paginação
-	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+
+	// Buscar categorias com paginação
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
-		
+
 	}
-	
-	//Método auxiliar para transformar um DTO em uma Entidade
-	//Instancia uma Cliente através de um DTO
+
+	// Método auxiliar para transformar um DTO em uma Entidade
+	// Instancia uma Cliente através de um DTO
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+
 	}
 }
