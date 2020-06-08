e0428ec5baffedfe8531fab66d6070b7c5a19022
¨Levi <levilude@gmail.com>
2020-06-07 23:31:08 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/lude/sisempresa/dto/ClienteNewDTO.java
+++ src/main/java/com/lude/sisempresa/dto/ClienteNewDTO.java
@@ -1,25 +1,49 @@
 package com.lude.sisempresa.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.lude.sisempresa.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO {
 
 	//Dados do cliente
+	@NotEmpty(message= "Preenchimento obrigatório")
+	@Length(min=5, max=120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message= "Preenchimento obrigatório")
+	@Email(message= "Email inválido")
 	private String email;
+	
+	@NotEmpty(message= "Preenchimento obrigatório")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
 	//Dados de endereço
+	@NotEmpty(message= "Preenchimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message= "Preenchimento obrigatório")
 	private String numero;
+	
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message= "Preenchimento obrigatório")
 	private String cep;
 	
 	//Dados da cidade que vai no endereço
 	private Integer cidadeId;
 
 	//Dados de telefone
+	@NotEmpty(message= "Preenchimento obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 	
